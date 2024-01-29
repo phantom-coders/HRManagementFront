24177596929176b7c8eca657ee9a91945d330ff7
Ruman-Islam <rumanislam48@gmail.com>
2024-01-29 23:02:34 +0600

    color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,41 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+
+      },
+      fontFamily: {
+
+      },
+      fontWeight: {
+
+      },
+      screens: {
+        xs: "300px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+      colors: {
+        primary: "#ACD7F2",
+        secondary: "#C9F2D7",
+      
+      },
+      padding: {
+   
+      },
+      keyframes: {
+
+      },
+      animation: {
+
+      },
+      backgroundImage: {
+      },
+    },
   },
   plugins: [],
   corePlugins: {
